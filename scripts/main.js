ab3e072c520097d9c2183062d94de10e6d3203e9
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-06-09 01:31:16 +0300

    add coord of mouse

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
-const catlink = document.querySelector('.cathouse');
 
-//const a = catlink.style.left;
-//let a = catlink.style.left;
+const catlink = document.querySelector('.cathouse');
 
 catlink.style.left = '40%';
 
@@ -19,12 +17,38 @@ const runlink = document.querySelector('.catanime');
 
 runlink.style.left = '40%';
 
+const wraperlink = document.querySelector('.wrapper2');
+const wraperstyle = window.getComputedStyle(wraperlink);
+let widthwrapper2 = wraperstyle.width;
+console.log(widthwrapper2); 
+   // let koord = wraperlink.style.width/2;
+   // console.log(koord);
+
+
+
+/*
+setInterval(function(){
+	y.textContent = window.getComputedStyle(runlink).left
+}, 60);
+*/
+
 runlink.addEventListener('mouseenter', (ev) => {
+       
     
-    if(runlink.style.left == '80%') {
-        runlink.style.left = '0%';
+    ypx = window.getComputedStyle(runlink).left;
+    //y = ypx.replace(/[\D]+/g, '');  - убрать все небуквенные элементы из записи
+    y = Number(ypx.replace("px", '')); //убрать px из значения
+    console.log('Мышку поймали в координате', y);
+
+    if(y >= 600) {
+        console.log('прыг налево');
+        runlink.style.left = '0px';
     } 
         else {
-        runlink.style.left = '80%';
+        console.log('прыг направо');
+        runlink.style.left = '1100px';
     }
 });
+
+
+
